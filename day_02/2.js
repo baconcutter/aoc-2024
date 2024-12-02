861bc0aa1a58dc230cac5aa7ff64fca837bd275e
Auke Speksnijder <baconcutter@gmail.com>
2024-12-02 10:49:59 +0100

    2

--- day_02/2.js
+++ day_02/2.js
@@ -9,7 +9,6 @@ const reports = allFileContents
         .map(x => parseInt(x))
     );
 
-let part1 = 0;
 
 function isSafe(r) {
     let increasing = r[1] >= r[0];
@@ -22,10 +21,12 @@ function isSafe(r) {
             return false;
         }
     }
-    log('safe', r);
     return true;
 }
 
+// part 1
+
+let part1 = 0;
 for (let i = 0; i < reports.length; i++) {
     if (isSafe(reports[i])) {
         part1++;
@@ -34,31 +35,17 @@ for (let i = 0; i < reports.length; i++) {
 log(part1);
 
 // part 2
-
 let part2 = 0;
-
 reportLoop: for (let i = 0; i < reports.length; i++) {
     const r = reports[i];
-    if (isSafe(r)) {
-        log('safe 1', i, r);
-        part2++;
-    } else {
-        let increasing = r[1] >= r[0];
-        for (let x = 1; x < r.length; x++) {
-            const prefV = r[x - 1];
-            const v = r[x];
-            const diff = increasing ? v - prefV : prefV - v;
-            if (diff < 1 || diff > 3) {
-                if(!isSafe(r.toSpliced(x-1, 1)) && isSafe(r.toSpliced(x, 1))){
-                    continue reportLoop;
-                }
-            }
-        }
-        log('safe 2 ', i);
-        part2++;  
-    }
 
-    
+    for (let x = 0; x < r.length; x++) {
+        const copy = r.toSpliced(x, 1);
+        if (isSafe(r) || isSafe(copy)) {
+            part2++;
+            continue reportLoop;
+        }
+    }
 }
 log(part2);
 
